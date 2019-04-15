d6288ca658e7624bfb9f0046b15c187c62649bd5
Allen Tsai <allentsai93@gmail.com>
2019-04-15 10:26:48 -0700

    changed main to a class component, will need multiple states possibly

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import styles from './styles/Button.module.css';
 
 type TParams = {
-    src: string
+    src: string;
+    event: (ev: any) => void;
 }
 
-const Button = ({ src } : TParams) => {
+const Button = ({ src, event } : TParams) => {
     return (
-        <span className={styles.Button}>
+        <span className={styles.Button} onClick={event}>
             <img src={src} />
         </span>
     )
--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,51 +1,67 @@
-import * as React from "react";
+import React, { Component } from "react";
 import API from "../service/api";
 import Post from "./Post";
 import styles from "./styles/Main.module.css";
-import HeadlinePost from "./HeadlinePost";
 import subtract from "../assets/sharp-delete_sweep-24px.svg";
 import add from "../assets/sharp-add_circle-24px.svg";
 import Button from "../components/Button";
 import spinner from "../assets/spinner.svg";
 import ListPosts from "./ListPosts";
 
-const Main: React.FC = () => {
-  const [redditData, setRedditData] = React.useState<null | []>([]);
-  const [loaded, setLoaded] = React.useState(false);
+type RedditPost = {
+  data: {
+    id: string;
+    title: string;
+    subreddit: string;
+    permalink: string;
+    url: string;
+    author: string;
+    created: number;
+    domain: string;
+    ups: number;
+    parsed: {
+      lead_image_url: string;
+    };
+  };
+};
 
-  React.useEffect(() => {
+type MyState = {
+  redditData: RedditPost[][];
+  loaded: boolean;
+};
+class Main extends Component<{}, MyState> {
+  state: MyState = {
+    redditData: [],
+    loaded: false
+  };
+
+  componentDidMount() {
     API.getData("http://localhost:3001/news/parse").then(data => {
-      setRedditData(data);
-      setLoaded(true);
+      this.setState(prevState => ({
+        redditData: [...prevState.redditData, data],
+        loaded: true
+      }));
     });
-  }, []);
+  }
 
-  return (
-    <>
-      {loaded ? (
-        <>
-          <div className={styles.container}>
-            <div className={styles.headlineCarousel}>
-              <div className={styles.headlinesContainer}>
-                {redditData!.slice(0, 1).map((post: any, i) => (
-                  <div key={post.data.id} className={styles.headline1}>
-                    <Post
-                      title={post.data.title}
-                      subreddit={post.data.subreddit}
-                      permalink={post.data.permalink}
-                      url={post.data.url}
-                      author={post.data.author}
-                      created={post.data.created}
-                      domain={post.data.domain}
-                      ups={post.data.ups}
-                      image={post.data.parsed.lead_image_url}
-                      headline
-                    />
-                  </div>
-                ))}
-                <div className={styles.subHeadlineContainer}>
-                  {redditData!.slice(1, 3).map((post: any, i) => (
-                    <div key={post.data.id}>
+  addPostsHandler = () => {
+    console.log('hit');
+  }
+
+  render() {
+    const listOfPosts = this.state.redditData.map((posts, i) => (
+      <ListPosts posts={posts} key={i} />
+    ));
+
+    return (
+      <>
+        {this.state.loaded ? (
+          <>
+            <div className={styles.container}>
+              <div className={styles.headlineCarousel}>
+                <div className={styles.headlinesContainer}>
+                  {this.state.redditData[0]!.slice(0, 1).map((post: any, i) => (
+                    <div key={post.data.id} className={styles.headline1}>
                       <Post
                         title={post.data.title}
                         subreddit={post.data.subreddit}
@@ -60,47 +76,49 @@ const Main: React.FC = () => {
                       />
                     </div>
                   ))}
+                  <div className={styles.subHeadlineContainer}>
+                    {this.state.redditData[0]!.slice(1, 3).map(
+                      (post: any, i) => (
+                        <div key={post.data.id}>
+                          <Post
+                            title={post.data.title}
+                            subreddit={post.data.subreddit}
+                            permalink={post.data.permalink}
+                            url={post.data.url}
+                            author={post.data.author}
+                            created={post.data.created}
+                            domain={post.data.domain}
+                            ups={post.data.ups}
+                            image={post.data.parsed.lead_image_url}
+                            headline
+                          />
+                        </div>
+                      )
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className={styles.listControlContainer}>
+                <div className={styles.buttonContainer}>
+                  <Button src={subtract} event={this.addPostsHandler}/>
+                </div>
+                <div className={styles.listContainer}>
+                  {listOfPosts}
+                </div>
+                <div className={styles.buttonContainer}>
+                  <Button src={add} event={this.addPostsHandler}/>
                 </div>
               </div>
             </div>
-            <div className={styles.listControlContainer}>
-              <div className={styles.buttonContainer}>
-                <Button src={subtract} />
-              </div>
-              <div className={styles.listContainer}>
-                {/* <ul className={styles.ul}>
-                  {redditData!.slice(3).map((post: any) => (
-                    <li className={styles.li} key={post.data.id}>
-                      <Post
-                        title={post.data.title}
-                        subreddit={post.data.subreddit}
-                        permalink={post.data.permalink}
-                        url={post.data.url}
-                        author={post.data.author}
-                        created={post.data.created}
-                        domain={post.data.domain}
-                        ups={post.data.ups}
-                        image={post.data.parsed.lead_image_url}
-                        headline={false}
-                      />
-                    </li>
-                  ))}
-                </ul> */}
-                <ListPosts posts={redditData!.slice(3)}/>
-              </div>
-              <div className={styles.buttonContainer}>
-                <Button src={add} />
-              </div>
-            </div>
+          </>
+        ) : (
+          <div className={styles.loadingContainer}>
+            <img src={spinner} />
           </div>
-        </>
-      ) : (
-        <div className={styles.loadingContainer}>
-          <img src={spinner} />
-        </div>
-      )}
-    </>
-  );
-};
+        )}
+      </>
+    );
+  }
+}
 
 export default Main;
