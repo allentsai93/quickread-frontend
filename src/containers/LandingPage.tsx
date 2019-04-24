3f41c4c84cb7bb1834a2a80469584b6f4edabc1a
Allen Tsai <allentsai93@gmail.com>
2019-04-24 10:58:47 -0700

    added categories to landing page

--- src/containers/LandingPage.tsx
+++ src/containers/LandingPage.tsx
@@ -1,70 +1,152 @@
-import * as React from 'react';
-import styles from './styles/LandingPage.module.css';
-import API from '../service/api';
-import searchIcon from '../assets/search.svg';
-import { CSSTransition } from 'react-transition-group';
+import * as React from "react";
+import styles from "./styles/LandingPage.module.css";
+import API from "../service/api";
+import searchIcon from "../assets/search.svg";
+import { CSSTransition } from "react-transition-group";
 
+type Source = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  language: string;
+  country: string;
+};
 
 const LandingPage = () => {
-    const [sources, setSources] = React.useState<null | {id: string, name: string, description: string, category: string, language: string, country: string}>(null);
-    const [showSources, setShowSources] = React.useState(false);
-    const [showSubmit, setShowSubmit] = React.useState(false);
-    const [searchTerm, setSearchTerm] = React.useState('');
+  const [sources, setSources] = React.useState<null | Source[]>(null);
+  const [showSources, setShowSources] = React.useState(false);
+  const [showSubmit, setShowSubmit] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const categories = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology"
+  ];
+  const [selectedCategories, setSelectedCategories] = React.useState<string[]>(
+    []
+  );
 
-    React.useEffect(() => {
-        getSources();
-    }, []);
+  React.useEffect(() => {
+    getSources();
+  }, []);
 
-    const getSources = () => {
-        API.getData(`http://localhost:3001/sources`)
-        .then(data => {
-            setSources(data);
-        });
-    };
+  const getSources = () => {
+    API.getData(`http://localhost:3001/sources`).then(data => {
+      setSources(data.sources);
+    });
+  };
 
-    const searchHandler = (term: any) => {
-        setSearchTerm(term);
-        if(term.length >= 3) {
-            setShowSubmit(true);
-        } else {
-            setShowSubmit(false);
-        }
+  // const [selectedCategories, dispatch] = React.useReducer((selectedCategories, { type, value })  => {
+  //     switch(type) {
+  //         case 'add':
+  //             return [...selectedCategories, value];
+  //         case 'remove':
+  //             return selectedCategories.filter((_:null, index:any) => index !== value);
+  //         default:
+  //             return selectedCategories;
+  //     }
+  // }, []);
+
+  const searchHandler = (term: any) => {
+    setSearchTerm(term);
+    if (term.length >= 3) {
+      setShowSubmit(true);
+    } else {
+      setShowSubmit(false);
     }
+  };
 
-    const submitHandler = (e: any) => {
-        e.preventDefault();
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+  };
+
+  const handleSelectedCategory = (cat: string) => {
+    if (selectedCategories.includes(cat)) {
+      const popCat = [...selectedCategories];
+      popCat.splice(selectedCategories.indexOf(cat), 1);
+      setSelectedCategories(popCat);
+    } else {
+      setSelectedCategories([...selectedCategories, cat]);
     }
+  };
 
-    return (
-        <div className={styles.container}>
-            <h1>LandingPage</h1>
-            <div className={styles.inputContainer}>
-                <form>
-                    <input type={'text'} value={searchTerm} placeholder={'Search for a topic...'} onChange={e => searchHandler(e.target.value)}/>
-                    <CSSTransition
-                        in={showSubmit} 
-                        classNames={{ 
-                            exit: styles["exit"],
-                            enter: styles["enter"],
-                            enterActive: styles["enterActive"],
-                            exitActive: styles["exitActive"]
-                        }}
-                        timeout={1000}
-                        mountOnEnter
-                        unmountOnExit
-                        appear
-                    >
-                        <button type={'submit'} onSubmit={submitHandler}><img src={searchIcon}/></button>
-                    </CSSTransition>
-                </form>
-            </div>
-            <p>or</p>
-            <span className={!showSources ? styles.sourcesBtn : [styles.sourcesBtn, styles.sourcesBtnActive].join(' ')} onClick={() => setShowSources(!showSources)}>Choose a News Source</span>
-            <div className={!showSources ? styles.sourcesContainer : [styles.sourcesContainer, styles.sourcesContainerActive].join(' ')}>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <h1>LandingPage</h1>
+      <div className={styles.inputContainer}>
+        <form>
+          <input
+            type={"text"}
+            value={searchTerm}
+            placeholder={"Search for a topic..."}
+            onChange={e => searchHandler(e.target.value)}
+          />
+          <CSSTransition
+            in={showSubmit}
+            classNames={{
+              exit: styles["exit"],
+              enter: styles["enter"],
+              enterActive: styles["enterActive"],
+              exitActive: styles["exitActive"]
+            }}
+            timeout={1000}
+            mountOnEnter
+            unmountOnExit
+            appear
+          >
+            <button type={"submit"} onSubmit={submitHandler}>
+              <img src={searchIcon} />
+            </button>
+          </CSSTransition>
+        </form>
+      </div>
+      <p>or</p>
+      <span className={styles.sourcesBtn}>Choose Categories</span>
+      <div className={styles.sourcesContainer}>
+        {categories.map((cat, i) => {
+          return (
+            <button
+              className={styles.categoryButton}
+              key={cat + i}
+              onClick={() => handleSelectedCategory(cat)}
+            >
+              {cat}
+            </button>
+          );
+        })}
+      </div>
+      {selectedCategories ? (
+        <form className={styles.filterForm}>
+          {selectedCategories.map((cat, i) => (
+            <fieldset key={i}>
+              <legend>{cat}</legend>
+              {sources!
+                .filter(source => source.category === cat)
+                .map((source, i) => {
+                  return (
+                    <div key={i} className={styles.checkboxInputDiv}>
+                      <span className={styles.checkbox}>
+                        <input
+                          type={"checkbox"}
+                          value={source.id}
+                          id={source.id}
+                        />
+                      </span>
+                      <label htmlFor={source.id}>{source.name}</label>
+                    </div>
+                  );
+                })}
+            </fieldset>
+          ))}
+        </form>
+      ) : null}
+    </div>
+  );
 };
 
 export default LandingPage;
