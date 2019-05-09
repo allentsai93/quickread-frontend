4a5f1db146e1aba1ec719120050de9db77426686
Allen Tsai <allentsai93@gmail.com>
2019-05-09 15:39:00 -0700

    added beta header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import styles from './styles/Header.module.css';
 const Header = () => {
     return (
         <header className={styles.header}>
-          <Link to="/" className={styles.logo}>Teeldr</Link>
+          <Link to="/" className={styles.logo}>Teeldr<span className={styles.beta}>Beta</span></Link>
         </header>
     )
 };
