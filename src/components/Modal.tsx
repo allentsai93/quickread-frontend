2b1a252d2e7d9935655a2ee34ce45a570e7846eb
Allen Tsai <allentsai93@gmail.com>
2019-05-09 15:28:30 -0700

    styling mobile

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styles from "./styles/Modal.module.css";
 import useGlobal from "../store";
-import spinner from '../assets/spinner.svg';
+import spinner from "../assets/spinner.svg";
 import { Carousel } from "react-responsive-carousel";
 
 const Modal = () => {
@@ -11,52 +11,66 @@ const Modal = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.modal} style={{backgroundImage: `url(${modalContent.lead_image_url})`}}>
-        { modalLoaded ?
-          <>
+        <div
+          className={styles.modal}
+          style={{ backgroundImage: `url(${modalContent.lead_image_url})` }}
+        >
           <span
             className={styles.closeModal}
             onClick={() => globalActions.modalContent.closeModal()}
           >
             X
           </span>
-          <img className={styles.image} src={modalContent.lead_image_url} alt="article image" />
-          <div className={styles.content}>
-          <h1>{modalContent.title}</h1>
-          <h2>{modalContent.excerpt}</h2>
-          <div className={styles.contentStats}>
-            <a target="_blank" href={modalContent.url}>{modalContent.domain || "Source"}</a>
-            {!modalContent.error ? (
-              <p>Summarized Word Count: {modalContent.condensedWordCount}</p>
-            ) : null}
-            <p>Original Word Count: {modalContent.originalWordCount}</p>
-          </div>
-          {modalContent.error ? (
-            <div className={styles.contentContainer}>
-                  <h3>Original Article</h3>
-                  <p>{modalContent.content}</p>
-            </div>
+          {modalLoaded ? (
+            <>
+              <img
+                className={styles.image}
+                src={modalContent.lead_image_url}
+                alt="article image"
+              />
+              <div className={styles.content}>
+                <h1>{modalContent.title}</h1>
+                <h2>{modalContent.excerpt}</h2>
+                <div className={styles.contentStats}>
+                  <a target="_blank" href={modalContent.url}>
+                    {modalContent.domain || "Source"}
+                  </a>
+                  {!modalContent.error ? (
+                    <p>
+                      Summarized Word Count: {modalContent.condensedWordCount}
+                    </p>
+                  ) : null}
+                  <p>Original Word Count: {modalContent.originalWordCount}</p>
+                </div>
+                {modalContent.error ? (
+                  <div className={styles.contentContainer}>
+                    <h3>Original Article</h3>
+                    <p>{modalContent.content}</p>
+                  </div>
+                ) : (
+                  <Carousel
+                    showThumbs={false}
+                    showStatus={false}
+                    showArrows={false}
+                    showIndicators={false}
+                    centerMode={true}
+                    centerSlidePercentage={95}
+                  >
+                    <div className={styles.contentContainer}>
+                      <h3>Summarized Article</h3>
+                      <p>{modalContent.condensedContent}</p>
+                    </div>
+                    <div className={styles.contentContainer}>
+                      <h3>Original Article</h3>
+                      <p>{modalContent.content}</p>
+                    </div>
+                  </Carousel>
+                )}
+              </div>
+            </>
           ) : (
-            <Carousel
-              showThumbs={false}
-              showStatus={false}
-              showArrows={false}
-              showIndicators={false}
-              centerMode={true}
-              centerSlidePercentage={95}
-            >
-              <div className={styles.contentContainer}>
-                <h3>Summarized Article</h3>
-                <p>{modalContent.condensedContent}</p>
-              </div>
-              <div className={styles.contentContainer}>
-                <h3>Original Article</h3>
-                <p>{modalContent.content}</p>
-              </div>
-            </Carousel>
+            <img src={spinner} className={styles.loadingIcon} alt="loading" />
           )}
-        </div> 
-        </> :  <img src={spinner} className={styles.loadingIcon} alt="loading" />}
         </div>
       </div>
     </>
